eefe28e6caa17317eceb233fcef3e8adb3587d13
tulenkovR <kubmaster@mail.ru>
2018-09-27 00:51:15 +0300

    Add login

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,13 +1,35 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const keys = require('../../config/key');
 
-module.exports.login = (req, res) => {
-  res.status(200).json({
-    login: {
-      email: req.body.email,
-      password: req.body.password
+module.exports.login = async (req, res) => {
+  const userDb = await User.findOne({
+    email: req.body.email
+  });
+  if (userDb) {
+    const passwordResult = bcrypt.compareSync(req.body.password, userDb.password)
+    if (passwordResult) {
+      // token
+      const token = jwt.sign({
+        email: userDb.email,
+        userId: userDb._id
+      }, keys.jwt, {
+        expiresIn: 60 * 60
+      })
+      res.status(200).json({
+        token: `Bearer ${token}`
+      })
+    } else {
+      res.status(401).json({
+        message: 'Пароль не совпадает!'
+      })
     }
-  })
+  } else {
+    res.status(404).json({
+      message: 'User не найден!'
+    })
+  }
 };
 
 module.exports.register = async (req, res) => {
