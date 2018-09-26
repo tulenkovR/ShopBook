b955e4476b432e4f940dc43da1b4bf844ee09665
tulenkovR <kubmaster@mail.ru>
2018-09-26 16:08:25 +0300

    Add cors and morgan

--- api/app.js
+++ api/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
@@ -8,10 +11,12 @@ const positionRoutes = require('./routes/position');
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
