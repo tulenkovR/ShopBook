4878777f5143ba3c26c20ca638ec65e94772f318
tulenkovR <kubmaster@mail.ru>
2018-09-27 12:11:00 +0300

    Add methods for Category

--- api/controllers/category.js
+++ api/controllers/category.js
@@ -1,23 +1,47 @@
 const Category = require('../models/Category');
+const Position = require('../models/Position');
+const error = require('../utils/error');
 
-module.exports.getAll = (req, res) => {
-  res.json({
-    message: 'Test'
-  })
+module.exports.getAll = async (req, res) => {
+  try {
+    const categories = await Category.find({
+      user: req.user.id
+    });
+    res.status(200).json(categories);
+  } catch (e) {
+    error(res, e);
+  }
 };
 
-module.exports.getById = (req, res) => {
+module.exports.getById = async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.id);
+    res.status(200).json(category);
+  } catch (e) {
+    error(res, e);
+  }
+};
+
+module.exports.delete = async (req, res) => {
+  try {
+    await Category.remove({
+      _id: req.params.id
+    })
+    await Position.remove({
+      category: req.params.id
+    })
+    res.status(200).json({
+      message: 'Category delete'
+    });
+  } catch (e) {
+    error(res, e);
+  }
+};
+
+module.exports.create = async (req, res) => {
 
 };
 
-module.exports.delete = (req, res) => {
-
-};
-
-module.exports.create = (req, res) => {
-
-};
-
-module.exports.update = (req, res) => {
+module.exports.update = async (req, res) => {
 
 };
