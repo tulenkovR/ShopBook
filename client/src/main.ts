08dd66fc186773404fcf0a56439a18019c4c22c5
tulenkovR <kubmaster@mail.ru>
2018-09-27 13:58:50 +0300

    Add materialize

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,11 +3,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import 'materialize-css/dist/js/materialize.js';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .catch(err => console.error(err));
-
