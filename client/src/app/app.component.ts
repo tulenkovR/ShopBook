beb9620d3217ff1c69a208c2ee0003de07580bca
tulenkovR <kubmaster@mail.ru>
2018-10-01 23:58:32 +0300

    Add TokenInterceptor

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './shared/services/auth.service';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private auth: AuthService) {}
+  ngOnInit() {
+    const token = localStorage.getItem('auth-token');
+    if (token !== null) {
+      this.auth.setToken(token);
+    }
+  }
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
@@ -9,6 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
 import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
+import { TokenInterceptor } from './shared/classes/token.interceptor';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,13 @@ import { RegisterPageComponent } from './register-page/register-page.component';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      multi: true,
+      useClass: TokenInterceptor,
+    },
   ],
   bootstrap: [AppComponent],
 })
